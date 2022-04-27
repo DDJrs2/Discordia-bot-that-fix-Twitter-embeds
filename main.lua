b19c0aee46d8cd64161dddc02b90f9ff47b39641
DDJrs2 <deciodannerjr@gmail.com>
2022-04-27 00:05:57 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ require('TwitterFX');
 
 Client:on("messageCreate", function(message)
 	if not Client.user.bot then return end
-    if (FindTwitterAndStatus(message) and message.embed == nil and not message.author.bot) then
+    	if (FindTwitterAndStatus(message) and message.embed == nil and not message.author.bot) then
 
 		if not Client:waitFor("messageUpdate", 2000) then --This condition will be true if the message don't get an embed in 2000 milliseconds
 			message.channel:send(SendFXTwitterLink(message));
