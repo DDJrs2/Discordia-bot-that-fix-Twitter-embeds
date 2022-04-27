ff39a2a94e6068ece61f640ea864140ba7350748
DDJrs2 <deciodannerjr@gmail.com>
2022-04-27 00:03:09 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,82 +1,17 @@
 Discord = require('discordia');
 Client = Discord.Client();
 
-Client:on('ready', function()
-    print('Yeehaw');
-end)
+require('TwitterFX');
 
-local function isThisTwitter(rawContent, i)
-    return string.sub(rawContent, i, string.len("https://twitter.com")+i) == "https://twitter.com/";
-end
+Client:on("messageCreate", function(message)
+	if not Client.user.bot then return end
+    if (FindTwitterAndStatus(message) and message.embed == nil and not message.author.bot) then
 
-local function isThisFXTwitter(rawContent, i)
-    return string.sub(rawContent, i, string.len("https://fxtwitter.com")+i) == "https://fxtwitter.com/";
-end
+		if not Client:waitFor("messageUpdate", 2000) then --This condition will be true if the message don't get an embed in 2000 milliseconds
+			message.channel:send(SendFXTwitterLink(message));
+		end
 
-local function isThisStatus(rawContent, i)
-    return string.sub(rawContent, i, string.len("status")+i) == "status/";
-end
-
-local function FindTwitterAndStatus(Message)
-    local rawContent = Message.content;
-    local confirmation = false;
-
-    for i = 1, string.len(rawContent), 1 do
-        if isThisTwitter(rawContent, i) then
-            confirmation = true;
-            break;
-        end
-    end
-
-    if confirmation then
-        for i = 1, string.len(rawContent), 1 do
-            if isThisStatus(rawContent, i) then
-                return true;
-            end
-        end
-    else
-        return false;
-    end
-    
-    return false;
-end
-
-local function separateOnlyFXTwitterLink(entireMessage)
-    for i = 1, string.len(entireMessage), 1 do
-        if isThisFXTwitter(entireMessage, i) then
-            for j = i, string.len(entireMessage), 1 do
-                if string.sub(entireMessage, j, j) == " " then
-                    return string.sub(entireMessage, i, j);
-                end
-            end
-        end
-    end
-
-    for i = 1, string.len(entireMessage), 1 do
-        if isThisFXTwitter(entireMessage, i) then
-            return string.sub(entireMessage, i,-1);
-        end
-    end
-end
-
-local function TransformTwitterToFXTwitter(Message)
-    local rawContent = Message.content;
-
-    for i = 1, string.len(rawContent), 1 do
-        if isThisTwitter(rawContent, i) then
-            return separateOnlyFXTwitterLink(string.sub(rawContent, 1, i-1) .. "https://fxtwitter.com" .. string.sub(rawContent, i+string.len("https://twitter.com"), -1)); 
-        end
-    end
-end
-
-Client:on("messageCreate", function(Message)
-
-    if (FindTwitterAndStatus(Message) and Message.embed == nil and not Message.author.bot) then
-
-        Message.channel:send(TransformTwitterToFXTwitter(Message));
-
-    end
-
+	end
 end)
 
 local TokenFile = io.open("token.txt", "r");
