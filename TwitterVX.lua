d2602a745fe189f15420e430617130eabb0b6001
DDJrs2 <deciodannerjr@gmail.com>
2022-05-28 20:06:39 -0300

    fixed FX to VX

--- TwitterVX.lua
+++ TwitterVX.lua
@@ -2,17 +2,17 @@ local function isThisTwitter(twitterString, i)
 	return string.sub(twitterString, i, string.len("https://twitter.com")+i) == "https://twitter.com/";
 end
 
-local function isThisFXTwitter(twitterString, i)
-	return string.sub(twitterString, i, string.len("https://fxtwitter.com")+i) == "https://fxtwitter.com/";
+local function isThisVXTwitter(twitterString, i)
+	return string.sub(twitterString, i, string.len("https://vxtwitter.com")+i) == "https://vxtwitter.com/";
 end
 
 local function isThisStatus(twitterString, i)
 	return string.sub(twitterString, i, string.len("status")+i) == "status/";
 end
 
-local function separateOnlyFXTwitterLink(entiremessage)
+local function separateOnlyVXTwitterLink(entiremessage)
 	for i = 1, string.len(entiremessage), 1 do
-		if isThisFXTwitter(entiremessage, i) then
+		if isThisVXTwitter(entiremessage, i) then
 			for j = i, string.len(entiremessage), 1 do
 				if string.sub(entiremessage, j, j) == " " then
 					return string.sub(entiremessage, i, j);
@@ -22,7 +22,7 @@ local function separateOnlyFXTwitterLink(entiremessage)
 	end
 
 	for i = 1, string.len(entiremessage), 1 do
-    	if isThisFXTwitter(entiremessage, i) then
+    	if isThisVXTwitter(entiremessage, i) then
 			return string.sub(entiremessage, i,-1);
     	end
     end
@@ -61,12 +61,12 @@ function FindTwitterAndStatus(message)
 	end
 end
 
-function SendFXTwitterLink(message)
+function SendVXTwitterLink(message)
 	local rawContent = message.content;
 
 	for i = 1, string.len(rawContent), 1 do
 		if isThisTwitter(rawContent, i) then
-			local link = separateOnlyFXTwitterLink(string.sub(rawContent, 1, i-1) .. "https://fxtwitter.com" .. string.sub(rawContent, i+string.len("https://twitter.com"), -1));
+			local link = separateOnlyVXTwitterLink(string.sub(rawContent, 1, i-1) .. "https://vxtwitter.com" .. string.sub(rawContent, i+string.len("https://twitter.com"), -1));
 			local linkWithoutPhoto = removeLastBar(link);
 			if linkWithoutPhoto ~= nil then
 				return linkWithoutPhoto;
